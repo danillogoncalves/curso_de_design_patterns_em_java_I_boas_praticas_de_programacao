b651b35d2e4e2840ff1a828537b09a59f8e3c66a
Danillo <danillogoncalves001@gmail.com>
2023-06-13 20:21:27 -0300

    Aplicando o pattern Strategy

--- src/br/com/alura/loja/TesteImpostos.java
+++ src/br/com/alura/loja/TesteImpostos.java
@@ -1,7 +1,8 @@
 package br.com.alura.loja;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.Icms;
+import br.com.alura.loja.imposto.Iss;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -11,8 +12,8 @@ public class TesteImpostos {
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ICMS));
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+        System.out.println(calculadora.calcular(orcamento, new Icms()));
+        System.out.println(calculadora.calcular(orcamento, new Iss()));
     }
 
 }
