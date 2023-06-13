007d3ec96e94ec5d273a1e0c38b7eccbfb9a25d4
Danillo <danillogoncalves001@gmail.com>
2023-06-13 20:11:17 -0300

    Calculando impostos

---
+++ src/br/com/alura/loja/TesteImpostos.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TesteImpostos {
+
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+        System.out.println(calculadora.calcular(orcamento, TipoImposto.ICMS));
+        System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+    }
+
+}
